4fd7d181066950b547f1d3dd9cc7aa29c6a67f4a
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-05-12 03:05:34 +0800

    Slicing Login Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,29 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            theme: {
+                fontFamily: {
+                    poppins: "Poppins, sans-serif",
+                },
+                extend: {
+                    colors: {
+                        "alerange": '#FB6908',
+                        "gray-1": '#B4B4B4',
+                        "gray-2": '#E2E0E0',
+                        "form-bg": '#212121',
+                    },
+                    screens: {
+                        'laptopLg': '1160px',
+                        'laptopXl': '1360px',
+                    },
+                },
             },
-        },
-    },
 
     plugins: [forms],
 };
