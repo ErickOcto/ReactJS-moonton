6b4b7925ecffccfc7c7fea808b95a56ef801931c
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-05-13 19:14:40 +0800

    slicing movie card, complete slicing dashboard

--- resources/js/Layouts/Authenticated/Index.jsx
+++ resources/js/Layouts/Authenticated/Index.jsx
@@ -4,14 +4,14 @@ import Sidebar from "@/Layouts/Authenticated/Sidebar";
 export default function Authenticated({children}) {
   return (
       <>
-          <div class="mx-auto max-w-screen hidden lg:block">
+          <div className="mx-auto max-w-screen hidden lg:block">
               {/* Start:Sidebar */}
               <Sidebar/>
               {/* End:Sidebar */}
 
               {/* Start:Content */}
-                <div class="ml-[300px] px-[50px]">
-                    <div class="py-10 flex flex-col gap-[50px]">
+                <div className="ml-[300px] px-[50px]">
+                    <div className="py-10 flex flex-col gap-[50px]">
                         {/* Start:Top */}
                         <Topbar/>
                         {/* End:Top */}
@@ -20,8 +20,8 @@ export default function Authenticated({children}) {
                 </div>
               {/* End:Content */}
           </div>
-          <div class="mx-auto px-4 w-full h-screen lg:hidden flex bg-black">
-              <div class="text-white text-2xl text-center leading-snug font-medium my-auto">
+          <div className="mx-auto px-4 w-full h-screen lg:hidden flex bg-black">
+              <div className="text-white text-2xl text-center leading-snug font-medium my-auto">
                   Sorry, this page only supported on 1024px screen or above
               </div>
           </div>
